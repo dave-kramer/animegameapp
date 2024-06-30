84c2ce1b71a3889e346b8ca70dca90bcb9f4404c
Dave Kramer <87333067+dave-kramer@users.noreply.github.com>
2024-06-30 22:08:46 +0200

    styling, score feedback & timeout
    Gets feedback in a green & red color when an anime has been chosen.
    Added a timeout each round
    Added a divider & score in the middle of the screen
    Images are now a higher resolution & take up the whole screen

--- src/views/game.js
+++ src/views/game.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import topAnime from '../../assets/top_1000_anime.json'
+import topAnime from '../../assets/top_1000_anime.json';
 
 const getRandomAnime = (animeList, excludeId) => {
   const filteredList = animeList.filter(anime => anime.mal_id !== excludeId);
@@ -14,6 +13,9 @@ const Game = () => {
   const [nextAnime, setNextAnime] = useState(null);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
+  const [showScores, setShowScores] = useState(false);
+  const [imagePressed, setImagePressed] = useState(false);
+  const [scoreContainerColor, setScoreContainerColor] = useState('#298acd');
 
   useEffect(() => {
     const initialAnime = getRandomAnime(topAnime);
@@ -24,21 +26,37 @@ const Game = () => {
   const handleGuess = (isHigher) => {
     const isCorrect = nextAnime.score > currentAnime.score;
 
+    setShowScores(true);
+    setImagePressed(true);
+
     if (isHigher === isCorrect) {
       setScore(score + 1);
-      setCurrentAnime(nextAnime);
-      setNextAnime(getRandomAnime(topAnime, nextAnime.mal_id));
+      setScoreContainerColor('green');
     } else {
-      setGameOver(true);
+      setScoreContainerColor('red');
     }
-  };
 
+    setTimeout(() => {
+      if (isHigher === isCorrect) {
+        setCurrentAnime(nextAnime);
+        setNextAnime(getRandomAnime(topAnime, nextAnime.mal_id));
+      } else {
+        setGameOver(true);
+      }
+      setShowScores(false);
+      setImagePressed(false);
+      setScoreContainerColor('#298acd');
+    }, 2500);
+  };
+  
   const restartGame = () => {
     const initialAnime = getRandomAnime(topAnime);
     setCurrentAnime(initialAnime);
     setNextAnime(getRandomAnime(topAnime, initialAnime.mal_id));
     setScore(0);
     setGameOver(false);
+    setShowScores(false);
+    setScoreContainerColor('#298acd');
   };
 
   if (!currentAnime || !nextAnime) {
@@ -46,80 +64,104 @@ const Game = () => {
   }
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.container}>
-          {gameOver ? (
-            <View>
-              <Text style={styles.gameOverText}>Game Over! Your score: {score}</Text>
-              <TouchableOpacity style={styles.button} onPress={restartGame}>
-                <Text style={styles.buttonText}>Play Again</Text>
-              </TouchableOpacity>
-            </View>
-          ) : (
-            <View>
-              <View style={styles.animeContainer}>
-                <Text style={styles.animeText}>{currentAnime.title}</Text>
-                <TouchableOpacity onPress={() => handleGuess(false)}>
-                  <Image
-                    source={{ uri: currentAnime.image_url }}
-                    style={styles.animeImage}
-                  />
-                </TouchableOpacity>
-                <Text style={styles.scoreText}>Score: {score}</Text>
-              </View>
-              <View style={styles.animeContainer}>
-                <Text style={styles.animeText}>{nextAnime.title}</Text>
-                <TouchableOpacity onPress={() => handleGuess(true)}>
-                  <Image
-                    source={{ uri: nextAnime.image_url }}
-                    style={styles.animeImage}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
-          )}
+    <View style={styles.container}>
+      {gameOver ? (
+        <View>
+          <Text style={styles.gameOverText}>Game Over! Your score: {score}</Text>
+          <TouchableOpacity style={styles.button} onPress={restartGame}>
+            <Text style={styles.buttonText}>Play Again</Text>
+          </TouchableOpacity>
         </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      ) : (
+        <View style={styles.gameContainer}>
+          <View style={[styles.scoreContainer, { backgroundColor: scoreContainerColor }]}>
+            <Text style={styles.scoreText}>{score}</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.animeContainer}
+            onPress={() => !imagePressed && handleGuess(false)}
+          >
+            <Image
+              source={{ uri: currentAnime.image_url }}
+              style={styles.animeImage}
+            />
+            <Text style={styles.animeText}>{currentAnime.title}</Text>
+            {showScores && <Text style={styles.scoreTextOverlay}>Score: {currentAnime.score}</Text>}
+          </TouchableOpacity>
+          <View style={styles.divider} />
+          <TouchableOpacity
+            style={styles.animeContainer}
+            onPress={() => !imagePressed && handleGuess(true)}
+          >
+            <Image
+              source={{ uri: nextAnime.image_url }}
+              style={styles.animeImage}
+            />
+            <Text style={styles.animeText}>{nextAnime.title}</Text>
+            {showScores && <Text style={styles.scoreTextOverlay}>Score: {nextAnime.score}</Text>}
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#f8f8f8',
-  },
   container: {
+    flex: 1,
+  },
+  gameContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
   },
   animeContainer: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 20,
+    width: '100%',
   },
   animeImage: {
-    width: 200,
-    height: 300,
+    width: '100%',
+    height: '100%',
     resizeMode: 'cover',
-    borderRadius: 10,
   },
   animeText: {
     fontSize: 22,
     textAlign: 'center',
     position: 'absolute',
-    top: 100,
     color: '#fff',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: 5,
     borderRadius: 5,
-    zIndex: 10,
+  },
+  scoreTextOverlay: {
+    fontSize: 18,
+    textAlign: 'center',
+    position: 'absolute',
+    color: '#fff',
+    bottom: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 5,
+    borderRadius: 5,
+  },
+  divider: {
+    width: '100%',
+    height: 5,
+    backgroundColor: '#000',
+  },
+  scoreContainer: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 40,
+    zIndex: 1,
   },
   scoreText: {
-    fontSize: 18,
-    marginTop: 10,
+    fontSize: 24,
+    color: '#fff',
   },
   button: {
     padding: 15,
@@ -129,7 +171,8 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#fff',
-    fontSize: 18,
+    fontSize: 12,
+    textAlign: 'center',
   },
   gameOverText: {
     fontSize: 24,
